bfe52785708333d44c78131bceca7ee133456f55
Julian Knocke <Julian.Knocke@xing.com>
2014-11-03 16:36:15 +0000

    change tabs into spaces

--- chat.rb
+++ chat.rb
@@ -7,23 +7,23 @@ name = Etc.getlogin
 ctx = ZMQ::Context.new
 listener = ctx.socket(ZMQ::SUB)
 (2..254).each do |address|
-	listener.connect("tcp://192.168.43.#{address}:7315")
+  listener.connect("tcp://192.168.43.#{address}:7315")
 end
 listener.setsockopt(ZMQ::SUBSCRIBE, '')
 
 Thread.new do
-	loop do
-		s = ''
-		listener.recv_string(s)
-		puts "#{s.strip}"
-	end
+  loop do
+    s = ''
+    listener.recv_string(s)
+    puts "#{s.strip}"
+  end
 end.run
 
 talker = ctx.socket(ZMQ::PUB)
 talker.bind("tcp://192.168.43.159:7315")
 
 loop do
-	talker.send_string("<#{name}> #{gets}")
+  talker.send_string("<#{name}> #{gets}")
 end
 
 
