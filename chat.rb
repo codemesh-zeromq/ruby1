56671839f86ced5a034c119c46a088ce0a5d1d45
Julian Knocke <Julian.Knocke@xing.com>
2014-11-03 16:23:50 +0000

    fix some bugs

--- chat.rb
+++ chat.rb
@@ -2,28 +2,30 @@
 require 'bundler'
 Bundler.require
 
-ctx = ZMQ::Context.new
-subscriber = Thread.new do |t|
-	listener = ctx.socket(ZMQ::SUB)
-	listener.bind("tcp://*:7315")
-	listener.setsockopt(ZMQ::SUBSCRIBE, '')
+name = "julian"
 
+ctx = ZMQ::Context.new
+listener = ctx.socket(ZMQ::SUB)
+(2..254).each do |address|
+	listener.connect("tcp://192.168.43.#{address}:7315")
+end
+listener.setsockopt(ZMQ::SUBSCRIBE, '')
+
+Thread.new do
 	loop do
 		s = ''
 		listener.recv_string(s)
-		puts "Received: '#{s.strip}'"
+		puts "#{s.strip}"
 	end
-end
-subscriber.run
+end.run
 
 talker = ctx.socket(ZMQ::PUB)
-(1..254).each do |address|
-  talker.connect("tcp://192.168.43.#{address}:7315")
-end
+talker.bind("tcp://192.168.43.159:7315")
 
 loop do
-	msg = gets
-	talker.send_string(msg)
+	talker.send_string("<#{name}> #{gets}")
 end
+
+
 #83.218.151.42
 #10.151.2.106
