dc7b714455b9a59aa771d0b25f2455b0a5fac4b2
Julian Knocke <Julian.Knocke@xing.com>
2014-11-03 15:00:42 +0000

    identify server

--- server.rb
+++ server.rb
@@ -4,16 +4,18 @@ Bundler.require
 context = ZMQ::Context.new
 socket = context.socket(ZMQ::REP)
 socket.bind("tcp://*:7313")
+id = ARGV.first
+puts "server id: #{id}"
 
 loop do
 	request = ''
 	socket.recv_string(request)
 	puts "Received '#{request}'"
 	if request =~ /^hello$/i
-		socket.send_string 'world'
+		socket.send_string "world (#{id})"
 		puts "Sending 'world'"
 	else
-		socket.send_string 'nada'
+		socket.send_string "nada (#{id})"
 		puts "Nope"
 	end
 end
